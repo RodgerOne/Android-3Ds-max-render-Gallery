d3d9122fdb00895f45796cf56d66453481b12c8f
Roger <lafar996@gmail.com>
2017-11-19 00:29:27 +0100

    ViewPager
    wersja z elementem przed i za - sztuczna pętla

--- app/src/main/java/com/example/roger/pam_01/Gallery.java
+++ app/src/main/java/com/example/roger/pam_01/Gallery.java
@@ -1,13 +1,10 @@
 package com.example.roger.pam_01;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
-import android.widget.ImageView;
 import android.widget.ViewFlipper;
-
-import org.malcdevelop.cyclicview.CyclicAdapter;
-import org.malcdevelop.cyclicview.CyclicView;
 
 public class Gallery extends AppCompatActivity {
     private ViewFlipper vf;
@@ -16,9 +13,27 @@ public class Gallery extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_gallery);
-        final CyclicView vp = (CyclicView) findViewById(R.id.vp);
-        ImageAdapter im = new ImageAdapter(this, getIntent().getExtras().getInt("pos"));
+        final ViewPager vp = (ViewPager) findViewById(R.id.vp);
+        ImageAdapter im = new ImageAdapter(this);
         vp.setAdapter(im);
+        vp.setCurrentItem(getIntent().getExtras().getInt("pos")+1);
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 0) vp.setCurrentItem(MainScreen.mThumbIds.length, false);
+                else if(position == MainScreen.mThumbIds.length+1) vp.setCurrentItem(1,false);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         //vp.setChangePositionFactor(20);
     }
 }
