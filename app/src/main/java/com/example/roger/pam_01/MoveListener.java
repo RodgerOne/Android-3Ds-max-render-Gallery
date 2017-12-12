e965591a76103dd265789a58b1757f0a2529bd85
Rogerałke <228157@student.pwr.edu.pl>
2017-12-12 22:29:04 +0100

    wątki v2

--- app/src/main/java/com/example/roger/pam_01/MoveListener.java
+++ app/src/main/java/com/example/roger/pam_01/MoveListener.java
@@ -1,27 +1,41 @@
 package com.example.roger.pam_01;
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MoveListener implements View.OnTouchListener {
     private double _maxMultiplier, start_x, tmp_coef, min = 0.5, max = 2.5;
-    private static double _coef = 1.0;
-    Activity _a;
+    private static double _coef = 0.0;
+    private Activity _a;
 
     private DisplayMetrics metrics;
     private int w;
-    TextView tv;
+    private TextView tv;
+    private SeekBar sb;
+    private final MoveListener th = this;
+
 
     public static double getPosition(){return _coef;}
+
+    ValueAnimator anim;
 
     public MoveListener(Activity activity, double maxMultiplier){
         _a = activity;
         _maxMultiplier = maxMultiplier;
-
         tv = (TextView)_a.findViewById(R.id.move_pointer);
+        sb = (SeekBar)_a.findViewById(R.id.seekBar);
+        sb.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                th.onTouch(view, motionEvent);
+                return true;
+            }
+        });
         metrics = new DisplayMetrics();
         _a.getWindowManager().getDefaultDisplay().getMetrics(metrics);
         w = metrics.widthPixels;
@@ -36,17 +50,42 @@ public class MoveListener implements View.OnTouchListener {
                 break;
             case MotionEvent.ACTION_MOVE:
                 tmp_coef = (fx - start_x) / (w/2) * _maxMultiplier;
-                //tmp_coef = (tmp_coef > 0 && tmp_coef < min)? min : (tmp_coef < 0 && tmp_coef > -min)?  -min : tmp_coef;
                 tmp_coef = (tmp_coef < min && tmp_coef > -min)? 0 : tmp_coef;
                 if(tmp_coef > 0 )tmp_coef *=tmp_coef; else tmp_coef *=-tmp_coef;
                 tmp_coef = tmp_coef > max? max : tmp_coef < -max? -max : tmp_coef;
-                _coef = ((double)((int)(tmp_coef * 100)))/100;
+                //_coef = ((double)((int)(tmp_coef * 100)))/100;
+                tmp_coef = ((double)((int)(tmp_coef * 100)))/100;
+
+                //sb.setProgress((int)((_coef + max)*20));  // to byla zanim oddalem animacje
+                anim = ValueAnimator.ofInt((int)((_coef + max)*20), (int)((tmp_coef+max)*20));
+                anim.setDuration(250);
+                anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        int animProgress = (Integer) animation.getAnimatedValue();
+                        sb.setProgress(animProgress);
+                    }
+                });
+                anim.start();
+                _coef = tmp_coef;
+
                 tv.setText(String.valueOf(_coef));
                 if(!SlideShow.go) ((SlideShow)_a).start();
                 break;
             case MotionEvent.ACTION_UP:
-                //_coef = _coef > 0? min : -min;
+                anim = ValueAnimator.ofInt((int)((_coef + max)*20), (int)(max*20));
+                anim.setDuration(300);
+                anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        int animProgress = (Integer) animation.getAnimatedValue();
+                        sb.setProgress(animProgress);
+                    }
+                });
+                anim.start();
+
                 _coef = 0.0;
+                //sb.setProgress((int)((_coef + max)*20));
                 tv.setText(String.valueOf(_coef));
                 ((SlideShow)_a).stop();
                 break;
--- app/src/main/java/com/example/roger/pam_01/SlideShow.java
+++ app/src/main/java/com/example/roger/pam_01/SlideShow.java
@@ -1,18 +1,16 @@
 package com.example.roger.pam_01;
 import android.app.Activity;
-import android.content.AsyncTaskLoader;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.util.DisplayMetrics;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,26 +20,34 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class SlideShow extends Activity {
 
-    int w, h;
-    final int _framerate = 24;
-    final Integer[] frames = getIdOfDrawings();
     final Activity _a = this;
-
-    private int pos = 8;
-    private int q = 1000 / _framerate;
-
     Handler handler = new Handler();
-    public static boolean go = false;
-    TextView tv;
     LinkedBlockingQueue<Bitmap> buffer = new LinkedBlockingQueue<>();
+    final Integer[] frames = getIdOfDrawings();
+
+    final int _framerate = 24;
+    private static volatile int _step = 1;
+    private volatile int _position = 0;
+    private final int q = 1000 / _framerate;
+    private int border = frames.length;
+    private int w, h;
+
+    private double last_coef_buffer = 1, _coef_buffer = 1;
+
+    public static boolean go = false;
+    private volatile boolean runThreads = false;
+
+    private Thread th1, th2, th3;
+
+    TextView tv;
     ImageView img;
+    SeekBar sb;
+
     Runnable r = new Runnable(){
         @Override
         public void run() {
             if(go){
                 double tmp = MoveListener.getPosition();
-                if(tmp != 0.0 ){   if (tmp > 0) pos++; else pos --;}
-                pos = pos >= frames.length? 0 : pos <= 0? frames.length-1 : pos;
                 if(buffer.peek() != null && tmp!= 0.0)img.setImageBitmap(buffer.poll());
                 tv.setText(String.valueOf(buffer.size()));
                 start();
@@ -49,81 +55,22 @@ public class SlideShow extends Activity {
         }
     };
 
-    //3 watki, kazdy kolejny wlacza sie gdy drugi nie daje rady
-    ReentrantLock lock = new ReentrantLock();
-    volatile int _position = 0;
-    int border = frames.length;
-    private volatile boolean runThreads = true;
-    Thread th1 = new Thread(){
-        @Override
-        public void run() {
-            while(runThreads) {
-                if (buffer.size() < 16)
-                    try{
-                        if(lock.tryLock()){
-                            buffer.put(decodeSampledBitmapFromResource(frames[_position]));
-                            _position = (_position + 1) % border;
-                            lock.unlock();
-                        }
-                }catch (Exception e){continue;}
-                else try {sleep(11);} catch (Exception e) {continue;}
-
-            }
-        }
-    };
-    Thread th2 = new Thread(){
-        @Override
-        public void run() {
-            while(runThreads) {
-                if (buffer.size() < 10)
-                    try{
-                        if(lock.tryLock()){
-                            buffer.put(decodeSampledBitmapFromResource(frames[_position]));
-                            _position = (_position + 1) % border;
-                            lock.unlock();
-                        }
-                }catch (Exception e){continue;}
-                else try {sleep(8);} catch (Exception e) {continue;}
-
-            }
-        }
-    };
-    Thread th3 = new Thread(){
-        @Override
-        public void run() {
-            while(runThreads) {
-                if (buffer.size() < 5)
-                    try{
-                        if(lock.tryLock()){
-                            buffer.put(decodeSampledBitmapFromResource(frames[_position]));
-                            _position = (_position + 1) % border;
-                            lock.unlock();
-                        }
-                    }catch (Exception e){continue;}
-                else try {sleep(3);} catch (Exception e) {continue;}
-
-            }
-        }
-    };
-    /*
-    AsyncTaskLoader<Void> _mainImageLoader = new AsyncTaskLoader<Void>(_a) {
-        @Override
-        public Void loadInBackground() {
-            while(true) {
-                if (buffer.size() < 16)
-                    try{buffer.put(decodeSampledBitmapFromResource(frames[_position])); _position = (_position + 1) % _position;}catch (Exception e){continue;}
-                else try {wait();} catch (Exception e) {continue;}
-
-            }
-        }
-    };
-    */
+    //3 wątki, kazdy kolejny wlacza sie gdy drugi nie daje rady
+    ReentrantLock lock_position = new ReentrantLock(), lock_direction = new ReentrantLock();
     public void start(){
         go = true;
-        runThreads = true;
-        double tmp = Math.abs(MoveListener.getPosition());
+        double tmp = MoveListener.getPosition();
+        if(last_coef_buffer*tmp < 0 && lock_direction.tryLock() && lock_position.tryLock()){
+            _step *= -1;
+           _position += _step * buffer.size();
+           buffer.clear();
+           lock_position.unlock();
+           lock_direction.unlock();
+        }
+        if(tmp != 0)last_coef_buffer = tmp;
+        tmp = Math.abs(tmp);
         tmp = tmp == 0.0? 1.0 : tmp;
-       handler.postDelayed(r, (int)((double)q  / tmp));
+        handler.postDelayed(r, (int)((double)q  / tmp));
     }
     public void stop(){ go = false; }
 
@@ -134,28 +81,96 @@ public class SlideShow extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         getWindow().setStatusBarColor(Color.DKGRAY);
 
-        //_mainImageLoader.startLoading();
-
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         h = metrics.heightPixels - getStatusBarHeight();
         w = metrics.widthPixels;
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
         tv = (TextView)findViewById(R.id.buffer_size_pointer);
-
         img = (ImageView)findViewById(R.id.img);
-        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams((int)(w * 0.75), (int)(h*0.75));
+        sb = (SeekBar)findViewById(R.id.seekBar);
+
+        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams((int)(w * 0.75 / 1.28), (int)(h*0.75));
         params.leftToLeft = R.id.activity_slide_show;
         params.topToTop = R.id.activity_slide_show;
         params.rightToRight = R.id.activity_slide_show;
         params.bottomToBottom = R.id.activity_slide_show;
-        params.horizontalBias = 0.2f;
+        params.horizontalBias = 0.1f;
         params.verticalBias = 0.2f;
         img.setLayoutParams(params);
-        findViewById(R.id.activity_slide_show).setOnTouchListener(new MoveListener(this, 1.5));
+        img.setImageResource(frames[_position]);
+
+        findViewById(R.id.activity_slide_show).setOnTouchListener(new MoveListener(this, 2.25));
+
+        runThreads = true;
+        th1 = new Thread(){
+            @Override
+            public void run() {
+                while(runThreads) {
+                    if (buffer.size() < 16)
+                        try{
+                            if(lock_position.tryLock()){
+                                _position = (_position + _step) % border;
+                                if(_position < 0) _position = frames.length + _position;
+                                buffer.put(decodeSampledBitmapFromResource(frames[_position]));
+                                lock_position.unlock();
+                            }
+                        }catch (Exception e){continue;}
+                    else try {sleep(13);} catch (Exception e) {continue;}
+
+                }
+            }
+        };
+        th2 = new Thread(){
+            @Override
+            public void run() {
+                while(runThreads) {
+                    if (buffer.size() < 10)
+                        try{
+                            if(lock_position.tryLock()){
+                                _position = (_position + _step) % border;
+                                if(_position < 0) _position = frames.length + _position;
+                                buffer.put(decodeSampledBitmapFromResource(frames[_position]));
+                                lock_position.unlock();
+                            }
+                        }catch (Exception e){continue;}
+                    else try {sleep(12);} catch (Exception e) {continue;}
+
+                }
+            }
+        };
+        th3 = new Thread(){
+            @Override
+            public void run() {
+                while(runThreads) {
+                    if (buffer.size() < 5)
+                        try{
+                            if(lock_position.tryLock()){
+                                _position = (_position + _step) % border;
+                                if(_position < 0) _position = frames.length + _position;
+                                buffer.put(decodeSampledBitmapFromResource(frames[_position]));
+                                lock_position.unlock();
+                            }
+                        }catch (Exception e){continue;}
+                    else try {sleep(11);} catch (Exception e) {continue;}
+
+                }
+            }
+        };
         th1.start();
         th2.start();
         th3.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        buffer.clear();
+        runThreads = false;
     }
 
     public int getStatusBarHeight() {
@@ -189,12 +204,5 @@ public class SlideShow extends Activity {
         options.inPreferredConfig = Bitmap.Config.RGB_565;
         options.inDither = true;
         return BitmapFactory.decodeResource(_a.getResources(), resId, options);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        runThreads = false;
-
     }
 }
