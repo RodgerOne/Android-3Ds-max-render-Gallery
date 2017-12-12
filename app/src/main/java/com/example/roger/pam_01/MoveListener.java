dd89fc58feb50b397642def6fe7b9a437e74a6d8
Roger <lafar996@gmail.com>
2017-12-12 16:35:53 +0100

    wątki

--- app/src/main/java/com/example/roger/pam_01/MoveListener.java
+++ app/src/main/java/com/example/roger/pam_01/MoveListener.java
@@ -4,14 +4,16 @@ import android.app.Activity;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 
 public class MoveListener implements View.OnTouchListener {
-    private double _maxMultiplier, start_x, tmp_coef, min = 0.7;
+    private double _maxMultiplier, start_x, tmp_coef, min = 0.5, max = 2.5;
     private static double _coef = 1.0;
     Activity _a;
 
     private DisplayMetrics metrics;
     private int w;
+    TextView tv;
 
     public static double getPosition(){return _coef;}
 
@@ -19,6 +21,7 @@ public class MoveListener implements View.OnTouchListener {
         _a = activity;
         _maxMultiplier = maxMultiplier;
 
+        tv = (TextView)_a.findViewById(R.id.move_pointer);
         metrics = new DisplayMetrics();
         _a.getWindowManager().getDefaultDisplay().getMetrics(metrics);
         w = metrics.widthPixels;
@@ -36,12 +39,15 @@ public class MoveListener implements View.OnTouchListener {
                 //tmp_coef = (tmp_coef > 0 && tmp_coef < min)? min : (tmp_coef < 0 && tmp_coef > -min)?  -min : tmp_coef;
                 tmp_coef = (tmp_coef < min && tmp_coef > -min)? 0 : tmp_coef;
                 if(tmp_coef > 0 )tmp_coef *=tmp_coef; else tmp_coef *=-tmp_coef;
-
+                tmp_coef = tmp_coef > max? max : tmp_coef < -max? -max : tmp_coef;
                 _coef = ((double)((int)(tmp_coef * 100)))/100;
+                tv.setText(String.valueOf(_coef));
                 if(!SlideShow.go) ((SlideShow)_a).start();
                 break;
             case MotionEvent.ACTION_UP:
-                _coef = _coef > 0? min : -min;
+                //_coef = _coef > 0? min : -min;
+                _coef = 0.0;
+                tv.setText(String.valueOf(_coef));
                 ((SlideShow)_a).stop();
                 break;
         }
--- app/src/main/java/com/example/roger/pam_01/SlideShow.java
+++ app/src/main/java/com/example/roger/pam_01/SlideShow.java
@@ -1,17 +1,23 @@
 package com.example.roger.pam_01;
 import android.app.Activity;
+import android.content.AsyncTaskLoader;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.util.DisplayMetrics;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 public class SlideShow extends Activity {
@@ -26,7 +32,8 @@ public class SlideShow extends Activity {
 
     Handler handler = new Handler();
     public static boolean go = false;
-    ArrayList<Bitmap> buffer = new ArrayList<>();
+    TextView tv;
+    LinkedBlockingQueue<Bitmap> buffer = new LinkedBlockingQueue<>();
     ImageView img;
     Runnable r = new Runnable(){
         @Override
@@ -35,19 +42,88 @@ public class SlideShow extends Activity {
                 double tmp = MoveListener.getPosition();
                 if(tmp != 0.0 ){   if (tmp > 0) pos++; else pos --;}
                 pos = pos >= frames.length? 0 : pos <= 0? frames.length-1 : pos;
-                img.setImageBitmap(buffer.remove(0));
-                //img.setImageBitmap((decodeSampledBitmapFromResource(frames[pos])));
+                if(buffer.peek() != null && tmp!= 0.0)img.setImageBitmap(buffer.poll());
+                tv.setText(String.valueOf(buffer.size()));
                 start();
-                buffer.add((decodeSampledBitmapFromResource(frames[pos])));
             }
         }
     };
 
+    //3 watki, kazdy kolejny wlacza sie gdy drugi nie daje rady
+    ReentrantLock lock = new ReentrantLock();
+    volatile int _position = 0;
+    int border = frames.length;
+    private volatile boolean runThreads = true;
+    Thread th1 = new Thread(){
+        @Override
+        public void run() {
+            while(runThreads) {
+                if (buffer.size() < 16)
+                    try{
+                        if(lock.tryLock()){
+                            buffer.put(decodeSampledBitmapFromResource(frames[_position]));
+                            _position = (_position + 1) % border;
+                            lock.unlock();
+                        }
+                }catch (Exception e){continue;}
+                else try {sleep(11);} catch (Exception e) {continue;}
+
+            }
+        }
+    };
+    Thread th2 = new Thread(){
+        @Override
+        public void run() {
+            while(runThreads) {
+                if (buffer.size() < 10)
+                    try{
+                        if(lock.tryLock()){
+                            buffer.put(decodeSampledBitmapFromResource(frames[_position]));
+                            _position = (_position + 1) % border;
+                            lock.unlock();
+                        }
+                }catch (Exception e){continue;}
+                else try {sleep(8);} catch (Exception e) {continue;}
+
+            }
+        }
+    };
+    Thread th3 = new Thread(){
+        @Override
+        public void run() {
+            while(runThreads) {
+                if (buffer.size() < 5)
+                    try{
+                        if(lock.tryLock()){
+                            buffer.put(decodeSampledBitmapFromResource(frames[_position]));
+                            _position = (_position + 1) % border;
+                            lock.unlock();
+                        }
+                    }catch (Exception e){continue;}
+                else try {sleep(3);} catch (Exception e) {continue;}
+
+            }
+        }
+    };
+    /*
+    AsyncTaskLoader<Void> _mainImageLoader = new AsyncTaskLoader<Void>(_a) {
+        @Override
+        public Void loadInBackground() {
+            while(true) {
+                if (buffer.size() < 16)
+                    try{buffer.put(decodeSampledBitmapFromResource(frames[_position])); _position = (_position + 1) % _position;}catch (Exception e){continue;}
+                else try {wait();} catch (Exception e) {continue;}
+
+            }
+        }
+    };
+    */
     public void start(){
         go = true;
+        runThreads = true;
         double tmp = Math.abs(MoveListener.getPosition());
         tmp = tmp == 0.0? 1.0 : tmp;
-        handler.postDelayed(r, (int)((double)q  / tmp));
+       handler.postDelayed(r, (int)((double)q  / tmp));
     }
     public void stop(){ go = false; }
 
@@ -58,14 +134,14 @@ public class SlideShow extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         getWindow().setStatusBarColor(Color.DKGRAY);
 
-        for(int i=0; i<pos; i++){
-            buffer.add(decodeSampledBitmapFromResource(frames[pos]));
-        }
+        //_mainImageLoader.startLoading();
 
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         h = metrics.heightPixels - getStatusBarHeight();
         w = metrics.widthPixels;
+
+        tv = (TextView)findViewById(R.id.buffer_size_pointer);
 
         img = (ImageView)findViewById(R.id.img);
         ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams((int)(w * 0.75), (int)(h*0.75));
@@ -77,6 +153,9 @@ public class SlideShow extends Activity {
         params.verticalBias = 0.2f;
         img.setLayoutParams(params);
         findViewById(R.id.activity_slide_show).setOnTouchListener(new MoveListener(this, 1.5));
+        th1.start();
+        th2.start();
+        th3.start();
     }
 
     public int getStatusBarHeight() {
@@ -110,5 +189,12 @@ public class SlideShow extends Activity {
         options.inPreferredConfig = Bitmap.Config.RGB_565;
         options.inDither = true;
         return BitmapFactory.decodeResource(_a.getResources(), resId, options);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        runThreads = false;
+
     }
 }
